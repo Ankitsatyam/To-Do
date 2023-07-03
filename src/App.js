5298ffee3e5afb5e0e9465c375f0ae1c65427234
Ankit <114883565+Ankitsatyam@users.noreply.github.com>
2023-07-04 01:34:42 +0530

    Initial push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DoctorList from './Pages/DoctorList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+      <DoctorList/>
+    </div>
+  );
+}
+
+export default App;
